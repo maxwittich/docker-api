a662b75509c07d60c48cec1d0f17db88ecb99d85
Max Wittich <Max.Wittich@outlook.de>
2026-07-27 09:08:12 +0200

    Initial commit: Spring Boot and MySQL Docker setup

---
+++ src/main/java/dev/maxwittich/DockerApiApplication.java
@@ -0,0 +1,13 @@
+package dev.maxwittich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerApiApplication.class, args);
+	}
+
+}
